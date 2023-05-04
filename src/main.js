af60ef59a4ec109dcdc99ed3a4451e2a44a2f4e5
Pedram Katanchi <pedramkat@gmail.com>
2023-05-04 23:29:52 +0200

    css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import './style.css'
 import InstantSearch from 'vue-instantsearch/vue3/es';
 
-createApp(App)
+createApp(App,{props: ["index"]})
     .use(InstantSearch)
     .mount('#app')
