f91c6db6c33432434cc14619f5cbd0193741a212
Pedram Katanchi <pedramkat@gmail.com>
2023-05-04 14:34:00 +0200

    fiox

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }}
 })
