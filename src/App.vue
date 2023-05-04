bed78c428c5aaafc1ba9099b2c3b3d294c1c6716
Pedram Katanchi <pedramkat@gmail.com>
2023-05-04 13:19:13 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ais-instant-search
+    :search-client="searchClient"
+    index-name="huts"
+  >
+    <ais-search-box />
+    <ais-hits>
+      <template v-slot:item="{ item }">
+        <pre>{{ item.official_name }}</pre>
+      </template>
+    </ais-hits>
+  </ais-instant-search>
+</template>
+
+<script>
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+import "instantsearch.css/themes/algolia-min.css";
+
+
+export default {
+  data() {
+    return {
+      searchClient: instantMeiliSearch(
+        "http://meilisearch.local",
+        "masterKey"
+      ),
+    };
+  },
+};
+</script>
+
+
+<style>
+  pre {
+    background-color: white;
+  }
+</style>
