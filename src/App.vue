3d086dbdda4d4062dc35d6f70aef2f6bdac4cfce
Pedram Katanchi <pedramkat@gmail.com>
2023-05-05 10:22:21 +0200

    added

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,23 @@
     :search-client="searchClient"
     index-name="huts"
   >
-    <ais-search-box />
-    <ais-hits>
-      <template v-slot:item="{ item }">
-        <p><strong>{{ item.name }}</strong></p>
-        <div>Member: {{ item.member_name }} ({{ item.member_acronym }})</div>
-        <a :href="item.url" target="_blank">Link</a>
-      </template>
-    </ais-hits>
+    <div class="search-panel">
+      <div class="filter-panel">
+        <h2>Members</h2>
+        <ais-refinement-list attribute="member_name" />
+      </div>
+      <div class="result-panel">
+        <ais-search-box />
+        <ais-hits>
+          <template v-slot:item="{ item }">
+            <p><strong>{{ item.name }}</strong></p>
+            <div>Elevation: {{ item.elevation }} m</div>
+            <div>Member: {{ item.member_name }} ({{ item.member_acronym }})</div>
+            <a :href="item.url" target="_blank">Link</a>
+          </template>
+        </ais-hits>
+      </div>
+    </div>
     <ais-pagination />
   </ais-instant-search>
 </template>
